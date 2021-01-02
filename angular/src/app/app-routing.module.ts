681733ffd5bf2c1f46090ba306c4855aefbc6c51
Urvashi <urvashih@cybage.com>
2021-01-02 18:21:39 +0530

    larangular

---
+++ angular/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SignupComponent } from './components/signup/signup.component';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ angular/src/app/components/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  public form = {
+    email: null,
+    password: null,
+  };
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    // console.log(this.form);
+    return this.http
+      .post('http://127.0.0.1:8000/api/login', this.form)
+      .subscribe(
+        (data) => console.log(data),
+        (error) => console.log(error)
+      );
+  }
+  ngOnInit(): void {}
+}
